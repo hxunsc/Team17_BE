191ecd6f74a9c7eee345004c66b287b5b80e3055
Woojae Jeong <woo6388@naver.com>
2024-10-28 18:05:02 +0900

    style: 오타 수정

--- src/main/java/homeTry/common/exception/CommonErrorType.java
+++ src/main/java/homeTry/common/exception/CommonErrorType.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorType implements ErrorType {
     METHOD_ARGUMENT_NOT_VALID_EXCEPTION("Common400_001", HttpStatus.BAD_REQUEST, "요청값에 대한 유효성 검사를 통과하지 못했습니다. 유효한 데이터를 입력해주세요"),
     ILLEGAL_ARGUMENT_EXCEPTION("Common400_002", HttpStatus.BAD_REQUEST, "IllegalArgumentException 발생"),
-    NO_SUCH_ARGUMENT_EXCEPTION("Common400_003", HttpStatus.NOT_FOUND, "요구하신 요청에 맞는 리소스를 찾을 수 없습니다."),
+    NO_SUCH_ARGUMENT_EXCEPTION("Common404_003", HttpStatus.NOT_FOUND, "요구하신 요청에 맞는 리소스를 찾을 수 없습니다."),
     MISSING_PATH_VARIABLE_EXCEPTION("Common400_004", HttpStatus.BAD_REQUEST, "PathVariable이 누락됐습니다."),
     MISSING_REQUEST_PARAM_EXCEPTION("Common400_005", HttpStatus.BAD_REQUEST, "RequestParameter가 누락됐습니다.");
 
