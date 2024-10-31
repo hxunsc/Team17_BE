a4067d16bb86621c0379ceea25fda434f31480ae
hxunsc <hyunsss21@naver.com>
2024-11-01 01:43:34 +0900

    refactor: 상품 이름, 상점 이름 유효성 검사 추가

--- src/main/java/homeTry/product/model/vo/ProductName.java
+++ src/main/java/homeTry/product/model/vo/ProductName.java
@@ -13,6 +13,10 @@ public record ProductName(String value) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("상품 이름은 필수입니다.");
         }
+
+        if (name.length() > 15) {
+            throw new IllegalArgumentException("상품 이름의 길이는 최대 15자 입니다");
+        }
     }
 
     @Override
--- src/main/java/homeTry/product/model/vo/StoreName.java
+++ src/main/java/homeTry/product/model/vo/StoreName.java
@@ -13,6 +13,10 @@ public record StoreName(String value) {
         if (storeName == null || storeName.isBlank()) {
             throw new IllegalArgumentException("상점 이름은 필수입니다.");
         }
+
+        if (storeName.length() > 15) {
+            throw new IllegalArgumentException("상점 이름의 길이는 최대 15자 입니다");
+        }
     }
 
     @Override
