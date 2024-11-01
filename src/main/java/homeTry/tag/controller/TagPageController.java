d6a0cd4d0b4d74e618ee923261868b2f596c85f7
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-11-01 21:32:09 +0900

    refactor: 페이지 경로 변경

--- src/main/java/homeTry/tag/controller/TagPageController.java
+++ src/main/java/homeTry/tag/controller/TagPageController.java
@@ -7,6 +7,7 @@ import homeTry.tag.teamTag.service.TeamTagService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,17 +24,23 @@ public class TagPageController {
         this.productTagService = productTagService;
     }
 
-    @GetMapping("/team")
-    public String showTeamTagList(Model model) {
-        List<ProductTagDto> productTags = productTagService.getProductTagList().productTags();
-        model.addAttribute("productTags", productTags);
-        return "ProductTags";
-    }
-
     @GetMapping("/product")
     public String showProductTagList(Model model) {
+        System.out.println("1");
+        List<ProductTagDto> productTags = productTagService.getProductTagList().productTags();
+        model.addAttribute("productTags", productTags);
+        return "tag/ProductTags";
+    }
+
+    @GetMapping("/team")
+    public String showTeamTagList(Model model) {
         List<TeamTagDTO> teamTags = teamTagService.getAllTeamTagList();
         model.addAttribute("teamTags", teamTags);
-        return "TeamTags";
+        return "tag/TeamTags";
+    }
+
+    @GetMapping("/team/add")
+    public String showProductTagForm() {
+        return "addProductTag";
     }
 }
