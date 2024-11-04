7b204cea447dab9c364354f88e63e76c70a8c4bb
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-11-04 13:38:31 +0900

    feat: 관리자 태그 페이지 태그 생성, 삭제 기능 추가

--- src/main/java/homeTry/tag/controller/TagPageController.java
+++ src/main/java/homeTry/tag/controller/TagPageController.java
@@ -1,13 +1,14 @@
 package homeTry.tag.controller;
 
 import homeTry.tag.productTag.dto.ProductTagDto;
+import homeTry.tag.productTag.dto.request.ProductTagRequest;
 import homeTry.tag.productTag.service.ProductTagService;
 import homeTry.tag.teamTag.dto.TeamTagDTO;
+import homeTry.tag.teamTag.dto.request.TeamTagRequest;
 import homeTry.tag.teamTag.service.TeamTagService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,9 +38,38 @@ public class TagPageController {
         return "tag/TeamTags";
     }
 
-    @GetMapping("/team/add")
+    @GetMapping("/product/add")
     public String showProductTagForm() {
-        return "addProductTag";
+        return "tag/AddProductTag";
+    }
+
+    @PostMapping("/product/save")
+    public String saveProductTag(@ModelAttribute ProductTagRequest productTagRequest) {
+        productTagService.addProductTag(productTagRequest);
+        return "redirect:/admin/tag/product"; // 상품 태그 목록 페이지로 리다이렉트
+    }
+
+    @GetMapping("/team/add")
+    public String showTeamTagFrom() {
+        return "tag/AddTeamTag";
+    }
+
+    @PostMapping("/team/save")
+    public String saveTeamTag(@ModelAttribute TeamTagRequest teamTagRequest) {
+        teamTagService.addTeamTag(teamTagRequest);
+        return "redirect:/admin/tag/team";
+    }
+
+    @DeleteMapping("/product/delete/{id}")
+    public String deleteProductTag(@PathVariable Long id) {
+        productTagService.deleteProductTag(id);
+        return "redirect:/admin/tag/product";
+    }
+
+    @DeleteMapping("/team/delete/{id}")
+    public String deleteTeamTag(@PathVariable Long id) {
+        teamTagService.deleteTeamTag(id);
+        return "redirect:/admin/tag/team";
     }
 
 }
