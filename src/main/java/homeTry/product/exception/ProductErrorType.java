f555256db1cd3841f8f99b63085dcbf525fb3784
hxunsc <hyunsss21@naver.com>
2024-11-05 22:22:10 +0900

    feat: 관리자 권한 확인 용 커스텀 예외 추가

--- src/main/java/homeTry/product/exception/ProductErrorType.java
+++ src/main/java/homeTry/product/exception/ProductErrorType.java
@@ -7,7 +7,11 @@ public enum ProductErrorType implements ErrorType {
 
     INVALID_MEMBER_EXCEPTION("Product400_001", HttpStatus.BAD_REQUEST, "유효하지 않은 회원입니다."),
 
-    PRODUCT_NOT_FOUND_EXCEPTION("Product404_001", HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다.");
+    PRODUCT_NOT_FOUND_EXCEPTION("Product404_001", HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다."),
+
+    UNAUTHORIZED_ACCESS_EXCEPTION("Product403_001", HttpStatus.FORBIDDEN, "관리자 권한이 필요합니다.");
+
+
 
     private final String errorCode;
     private final HttpStatus httpStatus;
