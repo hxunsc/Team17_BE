409d8af16b1dd56c9e03fa4b595b110a77f099ee
hxunsc <hyunsss21@naver.com>
2024-11-05 22:22:52 +0900

    refactor: 상품 삭제 soft delete 적용

--- src/main/java/homeTry/product/service/AdminProductService.java
+++ src/main/java/homeTry/product/service/AdminProductService.java
@@ -1,26 +1,31 @@
 package homeTry.product.service;
 
 import homeTry.member.dto.MemberDTO;
+import homeTry.member.service.MemberService;
 import homeTry.product.dto.request.ProductRequest;
 import homeTry.product.dto.response.ProductAdminResponse;
 import homeTry.product.exception.badRequestException.ProductNotFoundException;
+import homeTry.product.exception.badRequestException.UnauthorizedAccessException;
 import homeTry.product.model.entity.Product;
 import homeTry.product.repository.ProductRepository;
 import homeTry.tag.productTag.dto.ProductTagDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdminProductService {
 
     private final ProductRepository productRepository;
     private final ProductTagMappingService productTagMappingService;
+    private final MemberService memberService;
 
     public AdminProductService(ProductRepository productRepository,
-        ProductTagMappingService productTagMappingService) {
+        ProductTagMappingService productTagMappingService, MemberService memberService) {
         this.productRepository = productRepository;
         this.productTagMappingService = productTagMappingService;
+        this.memberService = memberService;
     }
 
     // 상품 추가
@@ -36,11 +41,20 @@ public class AdminProductService {
     }
 
     // 상품 삭제
+    @Transactional
     public void deleteProduct(Long productId, MemberDTO memberDTO) {
-        if (!productRepository.existsById(productId)) {
-            throw new ProductNotFoundException();
-        }
-        productRepository.deleteById(productId);
+        // 관리자 권한 확인
+        verifyAdmin(memberDTO);
+
+        Product product = productRepository.findById(productId)
+            .orElseThrow(ProductNotFoundException::new);
+
+        // 연관된 ProductTagMapping soft delete
+        productTagMappingService.setMappingsDeprecatedByProductId(productId);
+
+        // 해당 Product soft delete
+        product.markAsDeprecated();
+        productRepository.save(product);
     }
 
     // 상품 조회
@@ -50,6 +64,12 @@ public class AdminProductService {
                 ProductTagDto tagDto = productTagMappingService.getTagForProduct(product.getId());
                 return ProductAdminResponse.from(product, tagDto);
             });
+    }
+
+    private void verifyAdmin(MemberDTO memberDTO) {
+        if (!memberService.isAdmin(memberDTO.id())) {
+            throw new UnauthorizedAccessException();
+        }
     }
 
 
