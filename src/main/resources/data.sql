8ccabf8cc7826572bd3da2a14f63c0055be96919
rdme0 <we6610@gmail.com>
2024-11-07 20:22:46 +0900

    fix: data.sql 버그 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -62,14 +62,14 @@ values (1, '2024-10-09 03:00:10', 5400, false),
        (3, '2024-10-09 03:00:10', 3600, false),
        (4, '2024-10-08 03:00:20', 5400, false);
 
-insert into product (id, image_url, product_url, name, price, store_name, view_count)
-values (1, 'https://image10.coupangcdn.com/image/retail/images/1147911926604673-f01e3efb-c763-4601-a6f4-b02c377a97be.jpg', 'https://www.coupang.com/vp/products/7471278137?itemId=19495714190&vendorItemId=86605287776&src=1042503&spec=10304982&addtag=400&ctag=7471278137&lptag=10304982I19495714190&itime=20241031222941&pageType=PRODUCT&pageValue=7471278137&wPcid=17275845253493863363419&wRef=&wTime=20241031222941&redirect=landing&gclid=Cj0KCQjw1Yy5BhD-ARIsAI0RbXZiIjAHoyOj548lsnL_Bq6s72c7QwRgeKTJqmuqIyahbgzxMJiD_zgaAjzLEALw_wcB&mcid=f8fc9917542743f5b35b1439e91d61ba&campaignid=21638433336&adgroupid=', '런닝머신', 240000, 'coupang', 15),
-       (2, 'https://example.com/product2.jpg', 'https://example.com/product2', '실내자전거', 190000, 'coupang', 5),
-       (3, 'https://example.com/product3.jpg', 'https://example.com/product3', '폼롤러', 5000, 'adidas', 15),
-       (4, 'https://example.com/product4.jpg', 'https://example.com/product4', '요가매트', 24000, 'adidas', 8),
-       (5, 'https://example.com/product5.jpg', 'https://example.com/product5', '짐볼', 9900, 'body crew', 20),
-       (6, 'https://example.com/product6.jpg', 'https://example.com/product6', '요가 밴드', 7900, 'coupang', 11),
-       (7, 'https://example.com/product7.jpg', 'https://example.com/product7', '아몬드 브리즈 언스위트', 15550, 'coupang', 25);
+insert into product (id, image_url, product_url, name, price, store_name, view_count, is_deprecated)
+values (1, 'https://image10.coupangcdn.com/image/retail/images/1147911926604673-f01e3efb-c763-4601-a6f4-b02c377a97be.jpg', 'https://www.coupang.com/vp/products/7471278137?itemId=19495714190&vendorItemId=86605287776&src=1042503&spec=10304982&addtag=400&ctag=7471278137&lptag=10304982I19495714190&itime=20241031222941&pageType=PRODUCT&pageValue=7471278137&wPcid=17275845253493863363419&wRef=&wTime=20241031222941&redirect=landing&gclid=Cj0KCQjw1Yy5BhD-ARIsAI0RbXZiIjAHoyOj548lsnL_Bq6s72c7QwRgeKTJqmuqIyahbgzxMJiD_zgaAjzLEALw_wcB&mcid=f8fc9917542743f5b35b1439e91d61ba&campaignid=21638433336&adgroupid=', '런닝머신', 240000, 'coupang', 15, false),
+       (2, 'https://example.com/product2.jpg', 'https://example.com/product2', '실내자전거', 190000, 'coupang', 5, false),
+       (3, 'https://example.com/product3.jpg', 'https://example.com/product3', '폼롤러', 5000, 'adidas', 15, false),
+       (4, 'https://example.com/product4.jpg', 'https://example.com/product4', '요가매트', 24000, 'adidas', 8, false),
+       (5, 'https://example.com/product5.jpg', 'https://example.com/product5', '짐볼', 9900, 'body crew', 20, false),
+       (6, 'https://example.com/product6.jpg', 'https://example.com/product6', '요가 밴드', 7900, 'coupang', 11, false),
+       (7, 'https://example.com/product7.jpg', 'https://example.com/product7', '아몬드 브리즈 언스위트', 15550, 'coupang', 25, false);
 
 
 insert into product_tag (tag_name, is_deprecated)
