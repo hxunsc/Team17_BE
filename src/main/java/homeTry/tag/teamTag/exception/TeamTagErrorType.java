e0e8a03c58531285fabfd7c2a636eb47c3562331
Woojae Jeong <woo6388@naver.com>
2024-11-08 14:46:11 +0900

    style: 409 -> 400 상태코드 반환토록 오타 수정

--- src/main/java/homeTry/tag/teamTag/exception/TeamTagErrorType.java
+++ src/main/java/homeTry/tag/teamTag/exception/TeamTagErrorType.java
@@ -6,7 +6,7 @@ import homeTry.common.exception.ErrorType;
 
 public enum TeamTagErrorType implements ErrorType {
     //400
-    TEAM_TAG_ALREADY_EXISTS_EXCEPTION("TeamTag400_001", HttpStatus.CONFLICT, "이미 동일한 이름의 팀 태그가 존재합니다."),
+    TEAM_TAG_ALREADY_EXISTS_EXCEPTION("TeamTag400_001", HttpStatus.BAD_REQUEST, "이미 동일한 이름의 팀 태그가 존재합니다."),
 
     //404
     TEAM_TAG_NOT_FOUND_EXCEPTION("TeamTag404_001", HttpStatus.NOT_FOUND, "존재하지 않는 팀태그입니다.");
