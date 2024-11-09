3af03ebd8974c86a6600d9c8fc54b0064127c6a5
Woojae Jeong <woo6388@naver.com>
2024-11-09 23:25:32 +0900

    refactor: jpql 수정 및 findByTeamTag 메소드 구현

--- src/main/java/homeTry/team/repository/TeamTagMappingRepository.java
+++ src/main/java/homeTry/team/repository/TeamTagMappingRepository.java
@@ -1,5 +1,6 @@
 package homeTry.team.repository;
 
+import homeTry.tag.teamTag.model.entity.TeamTag;
 import homeTry.team.model.entity.Team;
 import homeTry.team.model.entity.TeamTagMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,9 +14,11 @@ public interface TeamTagMappingRepository extends JpaRepository<TeamTagMapping, 
     @Query("SELECT ttm " +
             "FROM TeamTagMapping ttm " +
             "WHERE ttm.team = :team " +
-            "AND ttm.teamTag.isDeprecated = false"
+            "AND ttm.isDeprecated = false"
     )
     List<TeamTagMapping> findByTeam(@Param("team") Team team);
 
     void deleteByTeam(Team team);
+
+    TeamTagMapping findByTeamTag(TeamTag teamTag);
 }
