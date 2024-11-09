69055b05da03933b81613345fb89125c16f8003d
hxunsc <hyunsss21@naver.com>
2024-11-09 23:03:49 +0900

    refactor: isDeprecated 부분 수정

--- src/main/java/homeTry/exerciseList/repository/ExerciseRepository.java
+++ src/main/java/homeTry/exerciseList/repository/ExerciseRepository.java
@@ -3,6 +3,7 @@ package homeTry.exerciseList.repository;
 import homeTry.exerciseList.model.entity.Exercise;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,5 +13,6 @@ public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
     void deleteByMemberId(Long memberId);
 
-    List<Exercise> findByDeprecatedFalse();
+    @Query("SELECT e FROM Exercise e WHERE e.isDeprecated = false")
+    List<Exercise> findByIsDeprecatedFalse();
 }
