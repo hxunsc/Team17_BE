bfb0635127422a71dae1d92450f5ce5e899068c9
hxunsc <hyunsss21@naver.com>
2024-11-09 23:16:00 +0900

    refactor: 컬럼명 명시 제거

--- src/main/java/homeTry/common/entity/SoftDeletableEntity.java
+++ src/main/java/homeTry/common/entity/SoftDeletableEntity.java
@@ -1,12 +1,10 @@
 package homeTry.common.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class SoftDeletableEntity extends BaseEntity {
 
-    @Column(name = "is_deprecated")
     private boolean isDeprecated;
 
     public boolean isDeprecated() {
