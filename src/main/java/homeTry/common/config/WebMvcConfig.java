7f7e3d856c969017d9530bfae16a6dda840c15a4
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-11-10 09:59:07 +0900

    refactor: css파일 jwtInterceptor 제외

--- src/main/java/homeTry/common/config/WebMvcConfig.java
+++ src/main/java/homeTry/common/config/WebMvcConfig.java
@@ -36,9 +36,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/api/oauth/**", "/admin/**");
-        //토큰 받는 경로 지정
+                .excludePathPatterns("/api/oauth/**",
+                        "/admin/**",
+                        "/static/**",       // 정적 리소스 제외
+                        "/resources/**",
+                        "/css/**");
 
+        //토큰 받는 경로 지정
         registry.addInterceptor(adminInterceptor)
                 .addPathPatterns("/admin/**");
     }
